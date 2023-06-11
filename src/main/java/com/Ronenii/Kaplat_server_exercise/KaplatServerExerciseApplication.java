8df8b4b35c6bf2fe41f13e9ccf8f2ad31df4e82a
Ronen Gelmanovich <ronenge@mta.ac.il>
2023-06-11 23:07:30 +0300

    Server done and docker done

--- src/main/java/com/Ronenii/Kaplat_server_exercise/KaplatServerExerciseApplication.java
+++ src/main/java/com/Ronenii/Kaplat_server_exercise/KaplatServerExerciseApplication.java
@@ -10,8 +10,6 @@ import org.springframework.context.annotation.Import;
 public class KaplatServerExerciseApplication {
 
 	public static void main(String[] args) {
-		String currentWorkingDir = System.getProperty("user.dir");
-		System.out.println("Current Working Directory: " + currentWorkingDir);
 		SpringApplication.run(KaplatServerExerciseApplication.class, args);
 	}
 
